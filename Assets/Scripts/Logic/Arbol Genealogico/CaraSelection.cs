08cad902122b01c49154fb2afec415f088577e92
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2023-02-04 00:01:11 +0100

    first scene for genealogic tree

---
+++ Assets/Scripts/Logic/Arbol Genealogico/CaraSelection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaraSelection : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _content;
+
+    public void SelectButton(GameObject selection)
+    {
+
+    }
+}
