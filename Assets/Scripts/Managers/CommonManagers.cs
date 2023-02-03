efa499c36138f292849c790f25f2bac324226b87
morax <m.moracuesta@gmail.com>
2023-02-04 00:45:40 +0100

    Saco función onfinished

--- Assets/Scripts/Managers/CommonManagers.cs
+++ Assets/Scripts/Managers/CommonManagers.cs
@@ -1,8 +1,12 @@
 using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class CommonManagers : Singleton
 {
+	[SerializeField]
+	protected PlayableDirector transitionPlayable;
 	private void Start()
 	{
 		if (!SceneManager.GetSceneByName("Game").isLoaded)
@@ -10,6 +14,7 @@ public class CommonManagers : Singleton
 			SceneManager.LoadScene("Game", LoadSceneMode.Additive);
 		}
 	}
+
 
 	public void GoToCreditsFromMainMenu()
 	{
@@ -35,15 +40,14 @@ public class CommonManagers : Singleton
 		StartCoroutine(GoToCoroutine());
 	}
 
+	public void OnFinishedGoToGameFromMainMenu()
+	{
+		SceneManager.UnloadSceneAsync("MainMenu");
+	}
+
 	public void GoToGameFromMainMenu()
 	{
-		IEnumerator GoToCoroutine()
-		{
-			yield return null;
-			SceneManager.UnloadSceneAsync("MainMenu");
-		}
-
-		StartCoroutine(GoToCoroutine());
+		transitionPlayable.Play();
 	}
 
 	public void GoToMainMenuFromGame()
